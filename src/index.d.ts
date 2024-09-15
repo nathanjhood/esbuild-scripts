47e7a73ef5f00a5db98c1f4c9007763215c942e0
StoneyDSP <nathanjhood@googlemail.com>
2024-09-15 23:58:47 +0200

    added 'src/index'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * @file index.d.ts
+ * @author Nathan J. Hood <nathanjhood@googlemail.com>
+ * @copyright 2024 MIT License
+ */
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+/**
+ * @file index.ts
+ * @author Nathan J. Hood <nathanjhood@googlemail.com>
+ * @copyright 2024 MIT License
+ */
