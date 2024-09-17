fe97ae6675dc42f03c3561caeb998b6c2e500f48
StoneyDSP <nathanjhood@googlemail.com>
2024-09-17 22:36:39 +0200

    edit 'test/run' - linted, formatted
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- test/run.ts
+++ test/run.ts
@@ -6,15 +6,15 @@
  * @copyright 2024 MIT License
  */
 
-import test = require("node:test");
-import assert = require("node:assert");
-import assertStrict = require("node:assert/strict");
-import reporters = require("node:test/reporters");
-import path = require("node:path");
-import os = require("node:os");
-import constants = require("node:constants");
+import test = require('node:test');
+import assert = require('node:assert');
+import assertStrict = require('node:assert/strict');
+import reporters = require('node:test/reporters');
+import path = require('node:path');
+import os = require('node:os');
+import constants = require('node:constants');
 
-import options = require("./setupTests");
+import options = require('./setupTests');
 
 type NodeTestRunnerParameters = Required<Parameters<typeof test.run>>;
 type NodeTestRunnerOptions = NodeTestRunnerParameters[0];
@@ -25,35 +25,38 @@ type NodeTestRunnerReturnType = ReturnType<typeof test.run>;
 const run = (process: NodeJS.Process) => {
   //
   process.on('uncaughtException', (error) => {
-    const e = new Error('uncaughtException', { cause: error })
+    const e = new Error('uncaughtException', { cause: error });
     // abortController.abort(e);
     process.exitCode = 1;
     throw error;
   });
   //
   process.on('unhandledRejection', (error) => {
-    const e = new Error('unhandledRejection', { cause: error })
+    const e = new Error('unhandledRejection', { cause: error });
     // abortController.abort(e);
     process.exitCode = 1;
     throw error;
   });
   //
   process.on('beforeExit', (code) => {
-    console.info('process', process.pid, 'exiting with code', code)
+    console.info('process', process.pid, 'exiting with code', code);
   });
   //
   process.on('exit', (code) => {
-    console.info('process', process.pid, 'exited with code', code)
+    console.info('process', process.pid, 'exited with code', code);
   });
-  if(process.env.NODE_ENV === undefined) throw new Error("'NODE_ENV' should be 'test', but it was undefined")
-  if(process.env.NODE_ENV !== "test") throw new Error("'NODE_ENV' should be 'test', but it was '" + process.env.NODE_ENV + "'")
+  if (process.env.NODE_ENV === undefined)
+    throw new Error("'NODE_ENV' should be 'test', but it was undefined");
+  if (process.env.NODE_ENV !== 'test')
+    throw new Error(
+      "'NODE_ENV' should be 'test', but it was '" + process.env.NODE_ENV + "'"
+    );
   //
   const testsStream = test.run(options);
   //
   return testsStream;
-}
-
+};
 
 if (require.main === module) {
-  run(process)
+  run(process);
 }
