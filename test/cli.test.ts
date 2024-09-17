601d66ecebc42f19ab20c1840e74e754acf44ff8
StoneyDSP <nathanjhood@googlemail.com>
2024-09-17 15:02:36 +0200

    edit 'test/cli' - added callbacks to 'done'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -1,18 +1,32 @@
 import test = require("node:test");
+import cliTestingLibrary = require("cli-testing-library");
 
-test.suite('cli', { timeout: 10000 }, (suiteContextA) => {
+const timeout = 10000;
+
+test.suite('cli', { timeout: timeout }, (suiteContext_cli) => {
   //
-  test.suite('imports', { timeout: 10000, signal: suiteContextA.signal }, (suiteContextB) => {
-    test.test('require', { timeout: 10000, signal: suiteContextB.signal }, (testContextA) => {
+  const mock = test.mock;
+  //
+  test.afterEach((ctx, done) => {
+    mock.restoreAll();
+    done();
+  }, { signal: suiteContext_cli.signal });
+  //
+  test.after((ctx, done) => {
+    mock.reset();
+    done();
+  }, { signal: suiteContext_cli.signal });
+  //
+  test.suite('imports', { timeout: timeout, signal: suiteContext_cli.signal }, (suiteContext_imports) => {
+    //
+    test.test('require', { timeout: timeout, signal: suiteContext_imports.signal }, (testContextA) => {
       return testContextA.assert.doesNotThrow(() => require('../src/cli'));
     });
-    test.test('import', { timeout: 10000, signal: suiteContextB.signal }, (testContextA) => {
-      return testContextA.assert.doesNotThrow(async () => await import('../src/cli'));
-    });
-    test.test('import', { timeout: 10000, signal: suiteContextB.signal }, (testContextA) => {
+    test.test('import', { timeout: timeout, signal: suiteContext_imports.signal }, (testContextA) => {
       return testContextA.assert.doesNotReject(import('../src/cli'));
     });
+    test.test('import (async)', { timeout: timeout, signal: suiteContext_imports.signal }, (testContextA) => {
+      return testContextA.assert.doesNotThrow(async () => await import('../src/cli'));
+    });
   });
-  //
-  test.suite.todo('types', { timeout: 10000, signal: suiteContextA.signal }, (suiteContextB) => { });
 })
