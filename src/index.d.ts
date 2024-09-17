7945b14b469b54534b9cf78cf4dc1a2fdf6431d2
StoneyDSP <nathanjhood@googlemail.com>
2024-09-17 22:38:34 +0200

    edit 'src/index' - begin exports
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,3 +3,23 @@
  * @author Nathan J. Hood <nathanjhood@googlemail.com>
  * @copyright 2024 MIT License
  */
+
+/**
+ *
+ */
+// import parseCommand = require("./process/parseCommand");
+// import parseArgV = require("./process/parseArgV");
+
+import type parseArgV from './process/parseArgV.d';
+import type parseCommand from './process/parseCommand.d';
+import type parseEnv from './process/parseEnv.d';
+
+export = index;
+
+declare interface Index {
+  parseCommand: parseCommand;
+  parseArgV: parseArgV;
+  parseEnv: parseEnv;
+}
+
+declare const index: Index;
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,22 @@
  * @author Nathan J. Hood <nathanjhood@googlemail.com>
  * @copyright 2024 MIT License
  */
+
+/**
+ *
+ */
+
+import parseCommand = require('./process/parseCommand');
+import parseArgV = require('./process/parseArgV');
+
+interface Index {
+  parseCommand: typeof parseCommand;
+  parseArgV: typeof parseArgV;
+}
+
+const index: Index = {
+  parseCommand: parseCommand,
+  parseArgV: parseArgV,
+};
+
+export = index;
