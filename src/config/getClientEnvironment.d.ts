e4d589b17f3a5784a91a5330abb0711baf2e4f53
StoneyDSP <nathanjhood@googlemail.com>
2024-09-22 17:49:48 +0200

    added 'src/getClientEvironment'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ src/config/getClientEnvironment.d.ts
@@ -0,0 +1,32 @@
+/**
+ * @file config/getClientEnvironment.d.ts
+ * @author Nathan J. Hood <nathanjhood@googlemail.com>
+ * @copyright 2024 MIT License
+ */
+
+/**
+ *
+ */
+export = getClientEnvironment;
+
+declare type GetClientEnvironmentOptions = {
+  verbose?: true | false;
+  debug?: true | false;
+};
+
+declare type GetClientEnvironmentResult = {
+  raw: NodeJS.ProcessEnv;
+  stringified: { 'process.env': NodeJS.ProcessEnv };
+};
+
+declare interface getClientEnvironment {
+  (proc: NodeJS.Process): GetClientEnvironmentResult;
+  (
+    proc: NodeJS.Process,
+    options?: GetClientEnvironmentOptions
+  ): GetClientEnvironmentResult;
+}
+
+declare const getClientEnvironment: getClientEnvironment;
+
+declare namespace getClientEnvironment {}
