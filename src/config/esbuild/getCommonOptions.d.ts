a7d734dc6f3dc9b410ed995afd4338d3c81b53e3
StoneyDSP <nathanjhood@googlemail.com>
2024-09-24 16:05:57 +0200

    added 'src/config/getCommonOptions' - factory for esbuild'd underlying Options object shared by Build and Transform APIs
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ src/config/esbuild/getCommonOptions.d.ts
@@ -0,0 +1,18 @@
+import type ESBuild = require('esbuild');
+
+export as namespace GetCommonOptions;
+
+export = getCommonOptions;
+
+declare interface getCommonOptions {
+  (proc: NodeJS.Process): ESBuild.CommonOptions;
+  (
+    proc: NodeJS.Process,
+    env: 'development' | 'production' | 'test'
+  ): ESBuild.CommonOptions;
+}
+
+declare const getCommonOptions: (
+  proc: NodeJS.Process,
+  env: 'development' | 'production' | 'test'
+) => ESBuild.CommonOptions;
