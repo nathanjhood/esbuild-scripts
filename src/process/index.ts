e1d31928beb87579500d305fbf85afe79895f5ca
StoneyDSP <nathanjhood@googlemail.com>
2024-09-25 06:18:14 +0200

    edit 'src/process/index' - add createRequire
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- src/process/index.ts
+++ src/process/index.ts
@@ -5,6 +5,9 @@
  */
 
 /** */
+import { createRequire } from 'node:module';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const require: NodeRequire = createRequire(__filename);
 
 import parseArgv = require('./parseArgv');
 import parseCommand = require('./parseCommand');
