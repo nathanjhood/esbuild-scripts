2ac247bdc9d892aff0475a94dd01661b62c13225
StoneyDSP <nathanjhood@googlemail.com>
2024-09-25 06:14:39 +0200

    edit 'src/config/esbuild/getCommonOptions' - add createRequire
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- src/config/esbuild/getCommonOptions.ts
+++ src/config/esbuild/getCommonOptions.ts
@@ -1,13 +1,14 @@
-import type FS = require('node:fs');
-import type Path = require('node:path');
+import { createRequire } from 'node:module';
+const require: NodeRequire = createRequire(__filename);
+
 import type ESBuild = require('esbuild');
 import fs = require('node:fs');
 import path = require('node:path');
 import getClientEnvironment = require('../getClientEnvironment');
 import getClientPaths = require('../getClientPaths');
 
-//
-import browsersList = require('browserslist');
+// //
+// import browsersList = require('browserslist');
 
 interface getCommonOptions {
   (
@@ -130,125 +131,125 @@ const getCommonOptions: getCommonOptions = (
 
 export = getCommonOptions;
 
-if (require.main === module) {
-  ((
-    proc: NodeJS.Process,
-    env: 'development' | 'production' | 'test',
-    options?: ESBuild.CommonOptions
-  ): ESBuild.CommonOptions => {
-    const commonOptions = getCommonOptions(
-      proc,
-      env,
-      options ? options : undefined
-    );
-    global.console.log(commonOptions);
-    return commonOptions;
-  })(global.process, 'development');
-}
+// if (require.main === module) {
+//   ((
+//     proc: NodeJS.Process,
+//     env: 'development' | 'production' | 'test',
+//     options?: ESBuild.CommonOptions
+//   ): ESBuild.CommonOptions => {
+//     const commonOptions = getCommonOptions(
+//       proc,
+//       env,
+//       options ? options : undefined
+//     );
+//     global.console.log(commonOptions);
+//     return commonOptions;
+//   })(global.process, 'development');
+// }
 
-//
-type Platform = 'browser' | 'node' | 'neutral';
-type Format = 'iife' | 'cjs' | 'esm';
-type Loader =
-  | 'base64'
-  | 'binary'
-  | 'copy'
-  | 'css'
-  | 'dataurl'
-  | 'default'
-  | 'empty'
-  | 'file'
-  | 'js'
-  | 'json'
-  | 'jsx'
-  | 'local-css'
-  | 'text'
-  | 'ts'
-  | 'tsx';
-type LogLevel = 'verbose' | 'debug' | 'info' | 'warning' | 'error' | 'silent';
-type Charset = 'ascii' | 'utf8';
-type Drop = 'console' | 'debugger';
-//
+// //
+// type Platform = 'browser' | 'node' | 'neutral';
+// type Format = 'iife' | 'cjs' | 'esm';
+// type Loader =
+//   | 'base64'
+//   | 'binary'
+//   | 'copy'
+//   | 'css'
+//   | 'dataurl'
+//   | 'default'
+//   | 'empty'
+//   | 'file'
+//   | 'js'
+//   | 'json'
+//   | 'jsx'
+//   | 'local-css'
+//   | 'text'
+//   | 'ts'
+//   | 'tsx';
+// type LogLevel = 'verbose' | 'debug' | 'info' | 'warning' | 'error' | 'silent';
+// type Charset = 'ascii' | 'utf8';
+// type Drop = 'console' | 'debugger';
+// //
 
-const sample: {
-  /** Documentation: https://esbuild.github.io/api/#sourcemap */
-  sourcemap?: boolean | 'linked' | 'inline' | 'external' | 'both';
-  /** Documentation: https://esbuild.github.io/api/#legal-comments */
-  legalComments?: 'none' | 'inline' | 'eof' | 'linked' | 'external';
-  /** Documentation: https://esbuild.github.io/api/#source-root */
-  sourceRoot?: string;
-  /** Documentation: https://esbuild.github.io/api/#sources-content */
-  sourcesContent?: boolean;
+// const sample: {
+//   /** Documentation: https://esbuild.github.io/api/#sourcemap */
+//   sourcemap?: boolean | 'linked' | 'inline' | 'external' | 'both';
+//   /** Documentation: https://esbuild.github.io/api/#legal-comments */
+//   legalComments?: 'none' | 'inline' | 'eof' | 'linked' | 'external';
+//   /** Documentation: https://esbuild.github.io/api/#source-root */
+//   sourceRoot?: string;
+//   /** Documentation: https://esbuild.github.io/api/#sources-content */
+//   sourcesContent?: boolean;
 
-  /** Documentation: https://esbuild.github.io/api/#format */
-  format?: ESBuild.Format;
-  /** Documentation: https://esbuild.github.io/api/#global-name */
-  globalName?: string;
-  /** Documentation: https://esbuild.github.io/api/#target */
-  target?: string | string[];
-  /** Documentation: https://esbuild.github.io/api/#supported */
-  supported?: Record<string, boolean>;
-  /** Documentation: https://esbuild.github.io/api/#platform */
-  platform?: ESBuild.Platform;
+//   /** Documentation: https://esbuild.github.io/api/#format */
+//   format?: ESBuild.Format;
+//   /** Documentation: https://esbuild.github.io/api/#global-name */
+//   globalName?: string;
+//   /** Documentation: https://esbuild.github.io/api/#target */
+//   target?: string | string[];
+//   /** Documentation: https://esbuild.github.io/api/#supported */
+//   supported?: Record<string, boolean>;
+//   /** Documentation: https://esbuild.github.io/api/#platform */
+//   platform?: ESBuild.Platform;
 
-  /** Documentation: https://esbuild.github.io/api/#mangle-props */
-  mangleProps?: RegExp;
-  /** Documentation: https://esbuild.github.io/api/#mangle-props */
-  reserveProps?: RegExp;
-  /** Documentation: https://esbuild.github.io/api/#mangle-props */
-  mangleQuoted?: boolean;
-  /** Documentation: https://esbuild.github.io/api/#mangle-props */
-  mangleCache?: Record<string, string | false>;
-  /** Documentation: https://esbuild.github.io/api/#drop */
-  drop?: ESBuild.Drop[];
-  /** Documentation: https://esbuild.github.io/api/#drop-labels */
-  dropLabels?: string[];
-  /** Documentation: https://esbuild.github.io/api/#minify */
-  minify?: boolean;
-  /** Documentation: https://esbuild.github.io/api/#minify */
-  minifyWhitespace?: boolean;
-  /** Documentation: https://esbuild.github.io/api/#minify */
-  minifyIdentifiers?: boolean;
-  /** Documentation: https://esbuild.github.io/api/#minify */
-  minifySyntax?: boolean;
-  /** Documentation: https://esbuild.github.io/api/#line-limit */
-  lineLimit?: number;
-  /** Documentation: https://esbuild.github.io/api/#charset */
-  charset?: ESBuild.Charset;
-  /** Documentation: https://esbuild.github.io/api/#tree-shaking */
-  treeShaking?: boolean;
-  /** Documentation: https://esbuild.github.io/api/#ignore-annotations */
-  ignoreAnnotations?: boolean;
+//   /** Documentation: https://esbuild.github.io/api/#mangle-props */
+//   mangleProps?: RegExp;
+//   /** Documentation: https://esbuild.github.io/api/#mangle-props */
+//   reserveProps?: RegExp;
+//   /** Documentation: https://esbuild.github.io/api/#mangle-props */
+//   mangleQuoted?: boolean;
+//   /** Documentation: https://esbuild.github.io/api/#mangle-props */
+//   mangleCache?: Record<string, string | false>;
+//   /** Documentation: https://esbuild.github.io/api/#drop */
+//   drop?: ESBuild.Drop[];
+//   /** Documentation: https://esbuild.github.io/api/#drop-labels */
+//   dropLabels?: string[];
+//   /** Documentation: https://esbuild.github.io/api/#minify */
+//   minify?: boolean;
+//   /** Documentation: https://esbuild.github.io/api/#minify */
+//   minifyWhitespace?: boolean;
+//   /** Documentation: https://esbuild.github.io/api/#minify */
+//   minifyIdentifiers?: boolean;
+//   /** Documentation: https://esbuild.github.io/api/#minify */
+//   minifySyntax?: boolean;
+//   /** Documentation: https://esbuild.github.io/api/#line-limit */
+//   lineLimit?: number;
+//   /** Documentation: https://esbuild.github.io/api/#charset */
+//   charset?: ESBuild.Charset;
+//   /** Documentation: https://esbuild.github.io/api/#tree-shaking */
+//   treeShaking?: boolean;
+//   /** Documentation: https://esbuild.github.io/api/#ignore-annotations */
+//   ignoreAnnotations?: boolean;
 
-  /** Documentation: https://esbuild.github.io/api/#jsx */
-  jsx?: 'transform' | 'preserve' | 'automatic';
-  /** Documentation: https://esbuild.github.io/api/#jsx-factory */
-  jsxFactory?: string;
-  /** Documentation: https://esbuild.github.io/api/#jsx-fragment */
-  jsxFragment?: string;
-  /** Documentation: https://esbuild.github.io/api/#jsx-import-source */
-  jsxImportSource?: string;
-  /** Documentation: https://esbuild.github.io/api/#jsx-development */
-  jsxDev?: boolean;
-  /** Documentation: https://esbuild.github.io/api/#jsx-side-effects */
-  jsxSideEffects?: boolean;
+//   /** Documentation: https://esbuild.github.io/api/#jsx */
+//   jsx?: 'transform' | 'preserve' | 'automatic';
+//   /** Documentation: https://esbuild.github.io/api/#jsx-factory */
+//   jsxFactory?: string;
+//   /** Documentation: https://esbuild.github.io/api/#jsx-fragment */
+//   jsxFragment?: string;
+//   /** Documentation: https://esbuild.github.io/api/#jsx-import-source */
+//   jsxImportSource?: string;
+//   /** Documentation: https://esbuild.github.io/api/#jsx-development */
+//   jsxDev?: boolean;
+//   /** Documentation: https://esbuild.github.io/api/#jsx-side-effects */
+//   jsxSideEffects?: boolean;
 
-  /** Documentation: https://esbuild.github.io/api/#define */
-  define?: { [key: string]: string };
-  /** Documentation: https://esbuild.github.io/api/#pure */
-  pure?: string[];
-  /** Documentation: https://esbuild.github.io/api/#keep-names */
-  keepNames?: boolean;
+//   /** Documentation: https://esbuild.github.io/api/#define */
+//   define?: { [key: string]: string };
+//   /** Documentation: https://esbuild.github.io/api/#pure */
+//   pure?: string[];
+//   /** Documentation: https://esbuild.github.io/api/#keep-names */
+//   keepNames?: boolean;
 
-  /** Documentation: https://esbuild.github.io/api/#color */
-  color?: boolean;
-  /** Documentation: https://esbuild.github.io/api/#log-level */
-  logLevel?: ESBuild.LogLevel;
-  /** Documentation: https://esbuild.github.io/api/#log-limit */
-  logLimit?: number;
-  /** Documentation: https://esbuild.github.io/api/#log-override */
-  logOverride?: Record<string, ESBuild.LogLevel>;
+//   /** Documentation: https://esbuild.github.io/api/#color */
+//   color?: boolean;
+//   /** Documentation: https://esbuild.github.io/api/#log-level */
+//   logLevel?: ESBuild.LogLevel;
+//   /** Documentation: https://esbuild.github.io/api/#log-limit */
+//   logLimit?: number;
+//   /** Documentation: https://esbuild.github.io/api/#log-override */
+//   logOverride?: Record<string, ESBuild.LogLevel>;
 
-  /** Documentation: https://esbuild.github.io/api/#tsconfig-raw */
-  tsconfigRaw?: string | ESBuild.TsconfigRaw;
-} = {} satisfies ESBuild.CommonOptions;
+//   /** Documentation: https://esbuild.github.io/api/#tsconfig-raw */
+//   tsconfigRaw?: string | ESBuild.TsconfigRaw;
+// } = {} satisfies ESBuild.CommonOptions;
