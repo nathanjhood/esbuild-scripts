27dfef6bfbe80147fe3be0d9d2428dcec0d67869
StoneyDSP <nathanjhood@googlemail.com>
2024-09-27 14:31:44 +0200

    edit 'src/scripts/build' - plugged in to ESBuild logLevel, added sync/async modes
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- src/scripts/build.ts
+++ src/scripts/build.ts
@@ -40,8 +40,41 @@ const build: build = async (
     }
   );
   //
+  proc.on('unhandledRejection', (err: unknown, origin: Promise<unknown>) => {
+    fs.writeSync(proc.stderr.fd, util.format(err, origin), null, 'utf8');
+    throw err;
+  });
+  //
 
-  const MAX_SAFE_INTEGER = 2147483647;
+  const logLevelValue: (logLevel: ESBuild.LogLevel) => number = (
+    logLevel: ESBuild.LogLevel
+  ): number => {
+    switch (logLevel) {
+      case 'silent': {
+        return 0;
+      }
+      case 'error': {
+        return 1;
+      }
+      case 'warning': {
+        return 2;
+      }
+      case 'info': {
+        return 3;
+      }
+      case 'debug': {
+        return 4;
+      }
+      case 'verbose': {
+        return 5;
+      }
+      default: {
+        throw new Error('No matching case in switch statement');
+      }
+    }
+  };
+
+  const MAX_SAFE_INTEGER: 2147483647 = 2147483647;
 
   //
   const console = new node_console.Console({
@@ -57,22 +90,9 @@ const build: build = async (
     // colorMode: 'auto', // cannot be used if using 'inspectOptions.colors'
   });
 
-  const {
-    assert: assert,
-    log: log,
-    info: info,
-    warn: warn,
-    error: error,
-    debug: debug,
-    time: time,
-    timeLog: timeLog,
-    timeEnd: timeEnd,
-  } = console;
-  //
-
   //
   const logName: string = 'esbuild-scripts build';
-  time(logName);
+  console.time(logName);
   //
 
   // //
@@ -85,93 +105,179 @@ const build: build = async (
   // ];
   // //
 
-  //
-  log(logName, 'log message');
-  info(logName, 'info message');
-  warn(logName, 'warn message');
-  error(logName, 'error message');
-  debug(logName, 'debug message');
-  assert(false, logName + ' assert message');
-  timeLog(logName);
-  //
+  const defaultBuildOptions: ESBuild.BuildOptions = getBuildOptions(
+    proc,
+    'production'
+  );
+
+  const buildOptions: ESBuild.BuildOptions = {
+    // defaults
+    ...defaultBuildOptions,
+    // args
+    ...options,
+  };
 
   const paths = getClientPaths(proc);
 
-  function copyPublicFolder() {
-    return fs.cpSync(paths.appPublic, paths.appBuild, {
-      dereference: true,
-      recursive: true,
-      // filter: (file) => file !== paths.appHtml, // TODO: HTML parser plugin
-    });
+  async function copyPublicFolder(): Promise<void> {
+    try {
+      return await new Promise<void>((resolvePublicDir) => {
+        return resolvePublicDir(
+          fs.cpSync(paths.appPublic, paths.appBuild, {
+            dereference: true,
+            recursive: true,
+          })
+        );
+      });
+    } catch (err) {
+      throw err;
+    }
   }
 
-  copyPublicFolder();
+  await copyPublicFolder();
 
   //
   return await esbuild
-    .build<ESBuild.BuildOptions>(getBuildOptions(proc, 'production'))
-    .then(
-      ({ warnings, errors, metafile, outputFiles, mangleCache }) => {
-        info('outputFiles:', outputFiles);
-        return { warnings, errors, metafile, outputFiles, mangleCache };
-      },
-      (err) => {
-        throw err;
-      }
-    )
-    .then(
-      ({ errors, warnings, metafile, outputFiles, mangleCache }) => {
+    .build<ESBuild.BuildOptions>(buildOptions)
+    .then<ESBuild.BuildResult<ESBuild.BuildOptions>>(
+      async ({
+        errors,
+        warnings,
+        metafile,
+        outputFiles,
+        mangleCache,
+      }: ESBuild.BuildResult<ESBuild.BuildOptions>) => {
         if (errors) {
-          const errorMessages = esbuild.formatMessagesSync(errors, {
+          const errorMessages = await esbuild.formatMessages(errors, {
             color: proc.stdout.isTTY,
             terminalWidth: 80,
-            kind: 'warning',
+            kind: 'error',
           });
-          errorMessages.forEach((e) => error(e));
+          if (
+            buildOptions.logLevel &&
+            logLevelValue(buildOptions.logLevel) <= 1
+          )
+            errorMessages.forEach((e) => console.error(e));
         }
-        return { errors, warnings, metafile, outputFiles, mangleCache };
+        return {
+          errors,
+          warnings,
+          metafile,
+          outputFiles,
+          mangleCache,
+        } satisfies ESBuild.BuildResult<ESBuild.BuildOptions>;
       },
       (err) => {
         throw err;
       }
     )
-    .then(
-      ({ errors, warnings, metafile, outputFiles, mangleCache }) => {
+    .then<ESBuild.BuildResult<ESBuild.BuildOptions>>(
+      async ({
+        errors,
+        warnings,
+        metafile,
+        outputFiles,
+        mangleCache,
+      }: ESBuild.BuildResult<ESBuild.BuildOptions>) => {
         if (warnings) {
-          const warningMessages = esbuild.formatMessagesSync(warnings, {
+          const warningMessages = await esbuild.formatMessages(warnings, {
             color: proc.stdout.isTTY,
             terminalWidth: 80,
             kind: 'warning',
           });
-          warningMessages.forEach((w) => warn(w));
+          if (
+            buildOptions.logLevel &&
+            logLevelValue(buildOptions.logLevel) <= 2
+          )
+            warningMessages.forEach((w) => console.warn(w));
         }
-        return { errors, warnings, metafile, outputFiles, mangleCache };
+        return {
+          errors,
+          warnings,
+          metafile,
+          outputFiles,
+          mangleCache,
+        } satisfies ESBuild.BuildResult<ESBuild.BuildOptions>;
       },
       (err) => {
         throw err;
       }
     )
-    .then(
-      ({ errors, warnings, metafile, outputFiles, mangleCache }) => {
-        if (mangleCache) {
-          debug(mangleCache);
-        }
-        return { errors, warnings, metafile, outputFiles, mangleCache };
-      },
-      (err) => {
-        throw err;
-      }
-    )
-    .then(
-      ({ errors, warnings, metafile, outputFiles, mangleCache }) => {
+    .then<ESBuild.BuildResult<ESBuild.BuildOptions>>(
+      async ({
+        errors,
+        warnings,
+        metafile,
+        outputFiles,
+        mangleCache,
+      }: ESBuild.BuildResult<ESBuild.BuildOptions>) => {
         if (metafile) {
-          const analysis = esbuild.analyzeMetafileSync(metafile, {
+          const analysis = await esbuild.analyzeMetafile(metafile, {
             color: proc.stdout.isTTY,
             verbose: true,
           });
-          log(analysis);
+          if (
+            buildOptions.logLevel &&
+            logLevelValue(buildOptions.logLevel) <= 3
+          )
+            console.log(analysis);
         }
-        return { errors, warnings, metafile, outputFiles, mangleCache };
+        return {
+          errors,
+          warnings,
+          metafile,
+          outputFiles,
+          mangleCache,
+        } satisfies ESBuild.BuildResult<ESBuild.BuildOptions>;
+      },
+      (err) => {
+        throw err;
+      }
+    )
+    .then<ESBuild.BuildResult<ESBuild.BuildOptions>>(
+      ({
+        warnings,
+        errors,
+        metafile,
+        outputFiles,
+        mangleCache,
+      }: ESBuild.BuildResult<ESBuild.BuildOptions>) => {
+        if (buildOptions.logLevel && logLevelValue(buildOptions.logLevel) <= 4)
+          console.info('outputFiles:', outputFiles);
+        return {
+          warnings,
+          errors,
+          metafile,
+          outputFiles,
+          mangleCache,
+        } satisfies ESBuild.BuildResult<ESBuild.BuildOptions>;
+      },
+      (err) => {
+        throw err;
+      }
+    )
+    .then<ESBuild.BuildResult<ESBuild.BuildOptions>>(
+      async ({
+        errors,
+        warnings,
+        metafile,
+        outputFiles,
+        mangleCache,
+      }: ESBuild.BuildResult<ESBuild.BuildOptions>) => {
+        if (mangleCache) {
+          if (
+            buildOptions.logLevel &&
+            logLevelValue(buildOptions.logLevel) <= 5
+          )
+            console.debug(mangleCache);
+        }
+        return {
+          errors,
+          warnings,
+          metafile,
+          outputFiles,
+          mangleCache,
+        } satisfies ESBuild.BuildResult<ESBuild.BuildOptions>;
       },
       (err) => {
         throw err;
@@ -181,41 +287,203 @@ const build: build = async (
       throw err;
     })
     .finally(() => {
-      timeEnd(logName);
+      console.timeEnd(logName);
     });
 };
 
-// const buildAsync: (
-//   proc: NodeJS.Process,
-//   options?: ESBuild.BuildOptions
-// ) => Promise<ESBuild.BuildResult<ESBuild.BuildOptions>> = util.promisify<
-//   NodeJS.Process,
-//   esbuild.BuildOptions | undefined,
-//   esbuild.BuildResult<esbuild.BuildOptions>
-// >(build);
-
-if (require.main === module) {
-  (async (
+interface buildSync {
+  (proc: NodeJS.Process): ESBuild.BuildResult<ESBuild.BuildOptions>;
+  (
     proc: NodeJS.Process,
     options?: ESBuild.BuildOptions
-  ): Promise<ESBuild.BuildResult<ESBuild.BuildOptions>> => {
+  ): ESBuild.BuildResult<ESBuild.BuildOptions>;
+}
+
+const buildSync: buildSync = (
+  proc: NodeJS.Process,
+  options?: ESBuild.BuildOptions
+): ESBuild.BuildResult<ESBuild.BuildOptions> => {
+  //
+  proc.on(
+    'uncaughtException',
+    (err: Error, origin: NodeJS.UncaughtExceptionOrigin) => {
+      fs.writeSync(proc.stderr.fd, util.format(err, origin), null, 'utf8');
+      throw err;
+    }
+  );
+  //
+
+  const logLevelValue: (logLevel: ESBuild.LogLevel) => number = (
+    logLevel: ESBuild.LogLevel
+  ): number => {
+    switch (logLevel) {
+      case 'silent': {
+        return 0;
+      }
+      case 'error': {
+        return 1;
+      }
+      case 'warning': {
+        return 2;
+      }
+      case 'info': {
+        return 3;
+      }
+      case 'debug': {
+        return 4;
+      }
+      case 'verbose': {
+        return 5;
+      }
+      default: {
+        throw new Error('No matching case in switch statement');
+      }
+    }
+  };
+
+  const MAX_SAFE_INTEGER: 2147483647 = 2147483647;
+
+  //
+  const console = new node_console.Console({
+    groupIndentation: 2,
+    ignoreErrors: options && options.logLevel === 'error' ? true : false,
+    stdout: proc.stdout,
+    stderr: proc.stderr,
+    inspectOptions: {
+      depth: MAX_SAFE_INTEGER,
+      breakLength: 80,
+      colors: options && options.color ? options.color : false,
+    },
+    // colorMode: 'auto', // cannot be used if using 'inspectOptions.colors'
+  });
+
+  //
+  const logName: string = 'esbuild-scripts build';
+  console.time(logName);
+  //
+
+  const defaultBuildOptions: ESBuild.BuildOptions = getBuildOptions(
+    proc,
+    'production'
+  );
+
+  const buildOptions: ESBuild.BuildOptions = {
+    // defaults
+    ...defaultBuildOptions,
+    // args
+    ...options,
+  };
+
+  const paths = getClientPaths(proc);
+
+  function copyPublicFolder(): void {
+    return fs.cpSync(paths.appPublic, paths.appBuild, {
+      dereference: true,
+      recursive: true,
+    });
+  }
+
+  copyPublicFolder();
+
+  const {
+    errors,
+    warnings,
+    metafile,
+    outputFiles,
+    mangleCache,
+  }: ESBuild.BuildResult<ESBuild.BuildOptions> =
+    esbuild.buildSync<ESBuild.BuildOptions>(buildOptions);
+
+  if (errors) {
+    const errorMessages = esbuild.formatMessagesSync(errors, {
+      color: proc.stdout.isTTY,
+      terminalWidth: 80,
+      kind: 'error',
+    });
+    if (buildOptions.logLevel && logLevelValue(buildOptions.logLevel) <= 1)
+      errorMessages.forEach((e) => console.error(e));
+  }
+
+  if (warnings) {
+    const warningMessages = esbuild.formatMessagesSync(errors, {
+      color: proc.stdout.isTTY,
+      terminalWidth: 80,
+      kind: 'warning',
+    });
+    if (buildOptions.logLevel && logLevelValue(buildOptions.logLevel) <= 2)
+      warningMessages.forEach((w) => console.warn(w));
+  }
+
+  if (metafile) {
+    const analysis = esbuild.analyzeMetafileSync(metafile, {
+      color: proc.stdout.isTTY,
+      verbose: true,
+    });
+    if (buildOptions.logLevel && logLevelValue(buildOptions.logLevel) <= 3)
+      console.log(analysis);
+  }
+
+  if (mangleCache) {
+    if (buildOptions.logLevel && logLevelValue(buildOptions.logLevel) <= 5)
+      console.debug(mangleCache);
+  }
+
+  if (buildOptions.logLevel && logLevelValue(buildOptions.logLevel) <= 4)
+    console.info('outputFiles:', outputFiles);
+
+  return {
+    errors,
+    warnings,
+    metafile,
+    outputFiles,
+    mangleCache,
+  } satisfies ESBuild.BuildResult;
+};
+
+// if (require.main === module) {
+//   (async (
+//     proc: NodeJS.Process,
+//     options?: ESBuild.BuildOptions
+//   ): Promise<ESBuild.BuildResult<ESBuild.BuildOptions>> => {
+//     //
+
+//     //
+//     return build(proc, options)
+//       .then((result: ESBuild.BuildResult<ESBuild.BuildOptions>) => {
+//         return result;
+//       })
+//       .catch((err) => {
+//         throw err;
+//       });
+
+//     //
+//   })(global.process, {
+//     logLevel: 'warning',
+//     metafile: true,
+//     write: false,
+//     color: true,
+//     platform: 'node',
+//     // color: global.process.stdout.hasColors(),
+//   });
+// }
+
+if (require.main === module) {
+  ((
+    proc: NodeJS.Process,
+    options?: ESBuild.BuildOptions
+  ): ESBuild.BuildResult<ESBuild.BuildOptions> => {
     //
 
     //
-    return build(proc, options)
-      .then((result: ESBuild.BuildResult<ESBuild.BuildOptions>) => {
-        return result;
-      })
-      .catch((err) => {
-        throw err;
-      });
+    return buildSync(proc, options);
 
     //
   })(global.process, {
-    logLevel: 'info',
+    logLevel: 'silent',
     metafile: true,
     write: false,
     color: true,
+    // platform: 'node',
     // color: global.process.stdout.hasColors(),
   });
 }
